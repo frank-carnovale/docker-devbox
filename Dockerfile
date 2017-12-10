f5ad5ef3986bbef2920a21ef8c33ed315781c33b
Frank Carnovale <frank.carnovale@gmail.com>
2017-12-10 18:24:59 +0000

    remove Paws; too heavy.  Also re-factor for Centos7 packages

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM centos:latest
 
 COPY builder.sh /
+COPY patches    /patches
 COPY GO         /usr/local/bin
 COPY devbox.sh  /etc/profile.d
 
--- builder.sh
+++ builder.sh
@@ -2,21 +2,96 @@
 
 yum -y install                                                                              \
     autoconf automake binutils bison flex gcc gcc-c++ gettext libtool make patch pkgconfig  \
-    redhat-rpm-config rpm-build rpm-sign git net-tools libaio expat-devel                   \
-    perl-Test-Exception perl-Test-Simple perl-Test-Warn perl-Carp-Clan perl-Class-Inspector \
-    perl-CPAN perl-DBD-Pg perl-DBD-SQLite perl-Env perl-Module-Build perl-TermReadKey       \
-    perl-Error perl-Git perl-YAML perl-DateTime perl-Module-Install                         \
-    perl-XML-Simple perl-XML-SAX
+    redhat-rpm-config rpm-build rpm-sign git net-tools libaio expat-devel
+
+yum -y install                  \
+    perl-Carp-Clan              \
+    perl-Class-Data-Inheritable \
+    perl-Class-Inspector        \
+    perl-Clone                  \
+    perl-CPAN                   \
+    perl-DateTime               \
+    perl-DBD-Pg                 \
+    perl-DBD-SQLite             \
+    perl-Devel-StackTrace       \
+    perl-Env                    \
+    perl-Exception-Class        \
+    perl-Error                  \
+    perl-Git                    \
+    perl-IO-Socket-IP           \
+    perl-Module-Build           \
+    perl-Module-Install         \
+    perl-Module-Runtime         \
+    perl-Scalar-List-Utils      \
+    perl-TermReadKey            \
+    perl-Test-Differences       \
+    perl-Test-Exception         \
+    perl-Test-Fatal             \
+    perl-Test-Requires          \
+    perl-Test-Simple            \
+    perl-Test-Tester            \
+    perl-Test-Warn              \
+    perl-XML-SAX                \
+    perl-XML-Simple             \
+    perl-YAML                   \
+
+
+yum -y install epel-release
+
+yum -y install                   \
+    perl-Algorithm-C3            \
+    perl-Capture-Tiny            \
+    perl-CLASS                   \
+    perl-Class-Accessor          \
+    perl-Class-Accessor-Chained  \
+    perl-Class-Method-Modifiers  \
+    perl-Class-XSAccessor        \
+    perl-Config-Any              \
+    perl-Context-Preserve        \
+    perl-Data-Dumper-Concise     \
+    perl-DateTime-Format-MySQL   \
+    perl-DateTime-Format-Oracle  \
+    perl-DateTime-Format-Pg      \
+    perl-Devel-GlobalDestruction \
+    perl-ExtUtils-Config         \
+    perl-ExtUtils-Helpers        \
+    perl-ExtUtils-InstallPaths   \
+    perl-Lingua-Stem-Snowball    \
+    perl-Module-Build-Tiny       \
+    perl-Module-Find             \
+    perl-MRO-Compat              \
+    perl-namespace-clean         \
+    perl-Path-Class              \
+    perl-Role-Tiny               \
+    perl-Scope-Guard             \
+    perl-Sub-Name                \
+    perl-TAP-Harness-Env         \
+    perl-Term-ReadLine-Gnu       \
+    perl-Test-Deep               \
+    perl-Test-Most               \
+    perl-Text-Unidecode          \
+
 
 cd /usr/local && mkdir {share,lib64}/perl5
 
-cpan install Term::ReadLine::Perl                                                \
-    DBIx::Class DBIx::Class::Schema::Loader DBIx::Class::Schema::Loader::Dynamic \
-    DateTime::Format::SQLite DateTime::Format::Pg Mojolicious                    \
-    Mojolicious::Plugin::Tables       \
-    Mojolicious::Plugin::OpenAPI      \
-    Mojolicious::Plugin::CORS         \
-    XML::SAX::Expat Paws              # !
+# temporary during probs with DBIx::Class::Schema::Loader..
+(
+    cd /patches
+    tar xzf Hash-Merge-0.200.tar.gz
+    cd      Hash-Merge-0.200 && cpan .
+)
+
+cpan install                             \
+    DBIx::Class                          \
+    DBIx::Class::Schema::Loader          \
+    DateTime::Format::Pg                 \
+    XML::SAX::Expat                      \
+    Mojolicious                          \
+    Mojolicious::Plugin::CORS            \
+    Mojolicious::Plugin::OpenAPI         \
+    Mojolicious::Plugin::StaticLog       \
+    DBIx::Class::Schema::Loader::Dynamic \
+    Mojolicious::Plugin::Tables          \
 
 mkdir /App
 
