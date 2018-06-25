23d9b6c5a4f7e51d5db1bf7d9fe94d248dc7a6c7
Frank Carnovale <frank.carnovale@sapient.com>
2018-06-25 11:01:51 +0100

    patch: fix prove -c colorisation

--- builder.sh
+++ builder.sh
@@ -94,6 +94,12 @@ cpan install                             \
     DBIx::Class::Schema::Loader::Dynamic \
     Mojolicious::Plugin::Tables          \
 
+# fix problem with 'prove' not honouring -c flag; will now colorise output in Jenkins..
+(
+  cd /patches
+  patch /usr/share/perl5/vendor_perl/TAP/Harness.pm rt-cpan-org-71850
+)
+
 mkdir /App
 
 chmod +x /usr/local/bin/GO
